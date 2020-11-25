5791d94029ece86d8fa586f5e34b5882239f334d
Patrice Laplante <plaplante@ferlab.bio>
2020-11-25 11:39:50 -0500

    fix(camemberts): #103/#104 file/directory structure reworked

--- src/packages/@cqdg/components/charts/index.js
+++ src/packages/@cqdg/components/charts/index.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2020 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of the GNU Affero General Public License v3.0.
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ */
+
 /* @flow */
 import React from 'react';
 import styled from '@ncigdc/theme/styled';
@@ -6,11 +26,11 @@ import {
   makeFilter,
   inCurrentFilters,
 } from '@ncigdc/utils/filters';
-import PieChart from '@ncigdc/components/Charts/PieChart';
+import PieChart from '@cqdg/components/charts/PieChart';
 import { Row, Column } from '@ncigdc/uikit/Flex';
 import { stringifyJSONParam } from '@ncigdc/utils/uri';
 import removeEmptyKeys from '@ncigdc/utils/removeEmptyKeys';
-import HorizontalBarChart from "../charts/HorizontalBarChart";
+import HorizontalBarChart from "./HorizontalBarChart";
 
 const toPieData = (clickHandler, docTypeSingular) => bucket => ({
   id: bucket.key,
