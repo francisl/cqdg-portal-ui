4a2f999ef0cc6ac40d16aa317dd99ad535f1e78a
Francis Lavoie <flavoie@ferlab.bio>
2020-12-02 17:07:09 -0500

    fix: #148 remove collapsable panel from donors search

--- src/packages/@cqdg/pages/FileRepository/CaseAggregations/CaseAggregations.js
+++ src/packages/@cqdg/pages/FileRepository/CaseAggregations/CaseAggregations.js
@@ -6,20 +6,15 @@ import {
   compose,
   setDisplayName,
   withPropsOnChange,
-  withState,
 } from 'recompose';
 
-import Modal from '@ncigdc/uikit/Modal';
-import FacetSelection from '@ncigdc/modern_components/FacetSelection';
 import FilterSearchInput from '@cqdg/components/inputs/FilterSearchInput';
 
 import FilterContainer from '@ferlab-ui/core/containers/filters/FilterContainer';
 import UploadSetButton from '@ncigdc/components/UploadSetButton';
-import { withTheme } from '@ncigdc/theme';
 import withFacetSelection from '@ncigdc/utils/withFacetSelection';
 import escapeForRelay from '@ncigdc/utils/escapeForRelay';
 import tryParseJSON from '@ncigdc/utils/tryParseJSON';
-import Header from '@ferlab-ui/core/containers/filters/FilterContainerHeader';
 
 import { UploadCaseSet } from '@ncigdc/components/Modals/UploadSet';
 import CaseIcon from '@ncigdc/theme/icons/Case';
@@ -27,81 +22,9 @@ import StackLayout from '@ferlab-ui/core/layouts/StackLayout';
 
 import t from '@cqdg/locales/intl';
 import features from '../../../../../features.json';
+import presetFacets from './CaseAggregationsFilters';
 
-const presetFacets = [
-  {
-    title: t('facet.study.name'),
-    field: 'study.short_name_keyword',
-    full: 'study.short_name_keyword',
-    type: 'keyword',
-  },
-  /* {
-    title: 'Study Id',
-    field: 'study.study_id_keyword',
-    full: 'study.study_id_keyword',
-    type: 'keyword',
-  }, */
-  {
-    title: t('facet.study.domain'),
-    field: 'study.domain',
-    full: 'study.domain',
-    type: 'keyword',
-  },
-  {
-    title: t('facet.gender'),
-    field: 'gender',
-    full: 'gender',
-    type: 'keyword',
-  },
-  {
-    title: t('facet.ethnicity'),
-    field: 'ethnicity',
-    full: 'ethnicity',
-    type: 'keyword',
-  },
-  {
-    title: t('facet.age.at.recruitment'),
-    field: 'age_at_recruitment',
-    full: 'age_at_recruitment',
-    type: 'long',
-  },
-  {
-    title: t('facet.age.at.diagnosis'),
-    field: 'diagnoses.age_at_diagnosis',
-    full: 'diagnoses.age_at_diagnosis',
-    type: 'long',
-  },
-  {
-    title: t('facet.vital.status'),
-    field: 'vital_status',
-    full: 'vital_status',
-    type: 'keyword',
-  },
-  {
-    title: t('facet.icd.category'),
-    field: 'diagnoses.icd_category_keyword',
-    full: 'diagnoses.icd_category_keyword',
-    type: 'keyword',
-  },
-  {
-    title: t('facet.mondo.term'),
-    field: 'diagnoses.mondo_term_keyword',
-    full: 'diagnoses.mondo_term_keyword',
-    type: 'keyword',
-  },
-  {
-    title: t('facet.hpo.category'),
-    field: 'phenotypes.hpo_category_keyword',
-    full: 'phenotypes.hpo_category_keyword',
-    type: 'keyword',
-  },
-  {
-    title: t('facet.hpo.term'),
-    field: 'phenotypes.hpo_term_keyword',
-    full: 'phenotypes.hpo_term_keyword',
-    type: 'keyword',
-  },
-];
+import '../Aggregations.css';
 
 const presetFacetFields = presetFacets.map((x) => x.field);
 const entityType = 'Cases';
@@ -112,83 +35,19 @@ const enhance = compose(
     entityType,
     presetFacetFields,
   }),
-  withTheme,
-  withState('caseIdCollapsed', 'setCaseIdCollapsed', false),
   withPropsOnChange(['viewer'], ({ viewer }) => ({
     parsedFacets: viewer.File ? tryParseJSON(viewer.File, {}) : {},
   }))
 );
 
-const styles = {
-  link: {
-    textDecoration: 'underline',
-    color: '#2a72a5',
-  },
-};
-
 const CaseAggregationsComponent = ({
-  caseIdCollapsed,
-  facetExclusionTest,
   handleRequestRemoveFacet,
-  handleResetFacets,
-  handleSelectFacet,
   parsedFacets,
   relay,
-  setCaseIdCollapsed,
-  setShouldShowFacetSelection,
-  shouldShowFacetSelection,
-  theme,
   userSelectedFacets,
   viewer,
 }) => (
   <div className="case-aggregations">
-    {features.caseAggregations && (
-      <div
-        className="text-right"
-        style={{
-          padding: '10px 15px',
-          borderBottom: `1px solid ${theme.greyScale5}`,
-        }}
-        >
-        {!!userSelectedFacets.length && (
-          <span>
-            <a onClick={handleResetFacets} style={styles.link}>
-              Reset
-            </a>
-            {' '}
-            &nbsp;|&nbsp;
-          </span>
-        )}
-        <a
-          onClick={() => setShouldShowFacetSelection(true)}
-          style={styles.link}
-          >
-          Add a Case/Biospecimen Filter
-        </a>
-      </div>
-    )}
-    {features.caseAggregations && (
-      <Modal
-        isOpen={shouldShowFacetSelection}
-        style={{
-          content: {
-            border: 0,
-            padding: '15px',
-            width: '65%',
-          },
-        }}
-        >
-        <FacetSelection
-          additionalFacetData={parsedFacets}
-          docType="cases"
-          excludeFacetsBy={facetExclusionTest}
-          isCaseInsensitive
-          onRequestClose={() => setShouldShowFacetSelection(false)}
-          onSelect={handleSelectFacet}
-          title="Add a Case/Biospecimen Filter"
-          />
-      </Modal>
-    )}
     {userSelectedFacets &&
       userSelectedFacets.map((facet) => (
         <FilterContainer
@@ -198,31 +57,15 @@ const CaseAggregationsComponent = ({
           key={facet.full}
           onRequestRemove={() => handleRequestRemoveFacet(facet)}
           relayVarName="repoCaseCustomFacetFields"
-          style={{ borderBottom: `1px solid ${theme.greyScale5}` }}
           />
       ))}
 
     {features.searchByCaseId && (
-      <Header
-        collapsed={caseIdCollapsed}
-        description="Enter UUID or ID of Case, Sample, Portion, Slide, Analyte or Aliquot"
-        field="cases.case_id"
-        setCollapsed={setCaseIdCollapsed}
-        title="Case"
-        />
-    )}
-    {features.searchByCaseId && (
       <FilterSearchInput
-        collapsed={caseIdCollapsed}
         doctype="cases"
         dropdownItem={(x) => (
           <StackLayout>
-            <CaseIcon
-              style={{
-                paddingRight: '1rem',
-                paddingTop: '1rem',
-              }}
-              />
+            <CaseIcon className="aggregations-search-icon" />
             <div>
               <div style={{ fontWeight: 'bold' }}>{x.submitter_donor_id}</div>
               <div style={{ fontSize: '80%' }}>
@@ -250,11 +93,6 @@ const CaseAggregationsComponent = ({
           query: { searchTableTab: 'cases' },
         }}
         idField="cases.case_id"
-        style={{
-          width: '100%',
-          borderBottom: `1px solid ${theme.greyScale5}`,
-          padding: '0 1.2rem 1rem',
-        }}
         type="case"
         UploadModal={UploadCaseSet}
         >
