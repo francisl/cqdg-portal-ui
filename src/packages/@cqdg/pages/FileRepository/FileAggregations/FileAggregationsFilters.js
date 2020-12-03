085b5ab89b53b7c9ee41e6c717bb31bb7648dc29
Francis Lavoie <flavoie@ferlab.bio>
2020-12-03 00:45:39 -0500

    added missing files

---
+++ src/packages/@cqdg/pages/FileRepository/FileAggregations/FileAggregationsFilters.js
@@ -0,0 +1,47 @@
+import t from '@cqdg/locales/intl';
+
+const presetFilters = [
+  {
+    title: t('facet.file'),
+    field: 'file_id',
+    full: 'file_id',
+    type: 'keyword',
+  },
+  {
+    field: 'data_category',
+    full: 'data_category',
+    type: 'keyword',
+  },
+  {
+    field: 'data_type',
+    full: 'data_type',
+    type: 'keyword',
+  },
+  {
+    field: 'is_harmonized',
+    full: 'is_harmonized',
+    type: 'keyword',
+  },
+  {
+    field: 'experimental_strategy',
+    full: 'experimental_strategy',
+    type: 'keyword',
+  },
+  {
+    field: 'file_format',
+    full: 'file_format',
+    type: 'keyword',
+  },
+  {
+    field: 'platform',
+    full: 'platform',
+    type: 'keyword',
+  },
+  {
+    field: 'data_access',
+    full: 'data_access',
+    type: 'keyword',
+  },
+];
+
+export default presetFilters;
