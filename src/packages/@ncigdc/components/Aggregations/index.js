d94d5e3c983a896b29381ad92b67ff14e82c2d9a
Patrice Laplante <67590246+plaplante-chust@users.noreply.github.com>
2020-12-07 16:19:29 -0500

    Fix/problem with short name keyword (#73)
    * fix: #157 i18n case facets

--- src/packages/@ncigdc/components/Aggregations/index.js
+++ src/packages/@ncigdc/components/Aggregations/index.js
@@ -50,9 +50,9 @@ export const facetFieldDisplayMapper = field => {
   const map = {
     'genes.symbol': 'Gene Symbol',
     'genes.gene_id': 'Gene',
-    'cases.case_id': 'Case',
+    'cases.case_id': 'Case'
   };
-  return map[field] || t(`aggregation.${field}`) || field;
+  return t(`aggregation.${map[field]}`) || t(`aggregation.${field}`) || map[field] || field;
 };
 
 export const BucketLink = styled(Link, {
